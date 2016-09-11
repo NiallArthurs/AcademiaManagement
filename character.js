dd06bdb4ff850204a15b02d1168db2b9f2a082be
Dipesh Amin <dipeshamin@gmail.com>
2016-09-11 20:12:36 +0100

    Move tile position and input handling to sprite (for use with non-character drawable objects e.g. computer)

--- character.js
+++ character.js
@@ -9,86 +9,72 @@ var Character = (function () {
     this.experiments = 0;
     this.theory = 0;
     this.computational = 0;
-    this.sprite = new Sprite(char, 32, 32*1.5,32*4,32*1.5*4,  animations, "walkdown");
-    var rpos = this.randomPosition();
-    this.x = rpos[0]  // Current y tile
-    this.y = rpos[1]; // Current y tile
-    this.Tx = 0; // target x tile (for blocking)
-    this.Ty = 0; // target y tile (for blocking)
     this.direction = 1; // 0 = up, 1 = down, 2 = left, 3 = right
     this.path = [];
     this.dt = 0;
     this.speed = 1; // Movement speed - pixels/second
     var self = this;
+    this.sprite = new Sprite(char, 32, 42,32*3,42*4,  animations, "walkdown", function() {self.input()});
+    var rpos = this.randomPosition();
+    this.sprite.x = rpos[0]  // Current y tile
+    this.sprite.y = rpos[1]; // Current y tile
     // subscribe to dt
     amplify.subscribe( "dt", function (data) {
       self.dt = data;
-    });
-
-    amplify.subscribe( "mousedown", function(ev) {
-      self.input(ev);
     });
   };
 
   Character.prototype = {
     input: function (data) {
-      var x = data["ev"].pageX - data["offsetLeft"];
-      var y = data["ev"].pageY - data["offsetTop"];
-
-      var xPos = cameraScreenPosition[0] + TILE_SIZE*(this.x - cameraMapPosition[0]);
-      var yPos = cameraScreenPosition[1] + TILE_SIZE*(this.y - cameraMapPosition[1])-(this.sprite.spriteHeight-TILE_SIZE);
-
-      if (x >= xPos && x <= (xPos+this.sprite.spriteWidth) && y >= yPos  && y <= (yPos+this.sprite.spriteHeight))
-      {
-        // Testing
-        this.randomMove();
-      }
+      // Testing
+      if (this.path.length == 0)
+      this.randomMove();
     },
     update: function () {
       // If the path isn't empty have the character follow it.
       if (this.path.length != 0)
       {
-        if (this.x == this.path[0][0] && this.y == this.path[0][1]) {
+        if (this.sprite.x == this.path[0][0] && this.sprite.y == this.path[0][1]) {
           this.path.shift();
         }
 
         if (this.path.length != 0) {
           // Move the character position along the path
-          if (this.x - this.path[0][0] > 0) {
-            if (this.x - (this.speed*this.dt) < this.path[0][0]) {
-              this.x = this.path[0][0];
+          if (this.sprite.x - this.path[0][0] > 0) {
+            if (this.sprite.x - (this.speed*this.dt) < this.path[0][0]) {
+              this.sprite.x = this.path[0][0];
             }
             else {
-              this.x -= (this.speed*this.dt);
+              this.sprite.x -= (this.speed*this.dt);
             }
             this.sprite.setState("walkleft");
             this.direction = 2;
           }
-          else if (this.x - this.path[0][0] < 0) {
-            if (this.x + (this.speed*this.dt) > this.path[0][0]) {
-              this.x = this.path[0][0];
+          else if (this.sprite.x - this.path[0][0] < 0) {
+            if (this.sprite.x + (this.speed*this.dt) > this.path[0][0]) {
+              this.sprite.x = this.path[0][0];
             }
             else {
-              this.x += (this.speed*this.dt);
+              this.sprite.x += (this.speed*this.dt);
             }
             this.sprite.setState("walkright");
             this.direction = 3;
           }
-          else if (this.y - this.path[0][1] > 0) {
-            if (this.y - (this.speed*this.dt) < this.path[0][1]) {
-              this.y = this.path[0][1];
+          else if (this.sprite.y - this.path[0][1] > 0) {
+            if (this.sprite.y - (this.speed*this.dt) < this.path[0][1]) {
+              this.sprite.y = this.path[0][1];
             } else {
-              this.y -= (this.speed*this.dt);
+              this.sprite.y -= (this.speed*this.dt);
             }
             this.sprite.setState("walkup");
             this.direction = 0;
           }
-          else if (this.y - this.path[0][1] < 0) {
-            if (this.y + (this.speed*this.dt) > this.path[0][1]) {
-              this.y = this.path[0][1];
+          else if (this.sprite.y - this.path[0][1] < 0) {
+            if (this.sprite.y + (this.speed*this.dt) > this.path[0][1]) {
+              this.sprite.y = this.path[0][1];
             }
             else {
-              this.y += (this.speed*this.dt);
+              this.sprite.y += (this.speed*this.dt);
             }
             this.sprite.setState("walkdown");
             this.direction = 1;
@@ -126,7 +112,7 @@ var Character = (function () {
       this.move(pos[0], pos[1]);
     },
     move: function(xDest, yDest) {
-      this.path = this.map.generatePath(this.x, this.y, xDest, yDest);
+      this.path = this.map.generatePath(this.sprite.x, this.sprite.y, xDest, yDest);
     },
   }
 
--- sprite.js
+++ sprite.js
@@ -1,11 +1,13 @@
 // Sprite class
 var Sprite = (function () {
 
-  var Sprite = function(_spritesheet, _swidth, _sheight, _width, _height, _animations, _state) {
+  var Sprite = function(_spritesheet, _swidth, _sheight, _width, _height, _animations, _state, _inputCallback) {
     this.spritesheet = _spritesheet;
     // Should get the below two from the image!
     this.width = _width;
     this.height = _height;
+    this.x = 0; // Tile x
+    this.y = 0; // Tile y
     this.spriteWidth = _swidth;
     this.spriteHeight = _sheight;
     this.animations = _animations;
@@ -13,13 +15,38 @@ var Sprite = (function () {
     this.time = 0;
     this.state = _state;
     this.frame = 0; //move along the animation
+    this.cameraMapPosition = 0;
+    this.cameraScreenPosition = 0;
+    this.inputCallback = _inputCallback;
     var self = this;
     amplify.subscribe( "dt", function (data) {
       self.tick(data);
     });
+    amplify.subscribe( "camera-pos", function (mPos, sPos) {
+      self.cameraPosition(mPos, sPos);
+    });
+    amplify.subscribe( "mousedown", function(ev) {
+      self.input(ev);
+    });
   };
 
   Sprite.prototype = {
+    input: function (data) {
+      // Sprite should handle mouse events.
+      if (typeof this.inputCallback  === "function") {
+          var x = data["ev"].pageX - data["offsetLeft"];
+          var y = data["ev"].pageY - data["offsetTop"];
+
+          var xPos = this.getX();
+          var yPos = this.getY();
+          if (x >= xPos && x <= (xPos+this.spriteWidth) && y >= yPos  && y <= (yPos+this.spriteHeight))
+            this.inputCallback(x, y);
+        }
+    },
+    cameraPosition: function(mPos, sPos) {
+      this.cameraMapPosition = mPos;
+      this.cameraScreenPosition = sPos;
+    },
     tick: function(dt) {
       this.time += dt;
       if (this.time >= this.animations[this.state]["speed"])
@@ -37,9 +64,15 @@ var Sprite = (function () {
       }
     },
     getX: function() {
-      return this.animations[this.state]["frames"][this.frame]*this.spriteWidth % this.width;
+      return cameraScreenPosition[0] + TILE_SIZE*(this.x - cameraMapPosition[0]);
     },
     getY: function() {
+      return cameraScreenPosition[1] + TILE_SIZE*(this.y - cameraMapPosition[1])-(this.spriteHeight-TILE_SIZE);
+    },
+    getSX: function() {
+      return this.animations[this.state]["frames"][this.frame]*this.spriteWidth % this.width;
+    },
+    getSY: function() {
       return this.spriteHeight*Math.floor(this.animations[this.state]["frames"][this.frame]/(this.width/this.spriteWidth));
     }
   }
