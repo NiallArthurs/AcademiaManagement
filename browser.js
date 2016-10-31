fb567a3fdd78f7e1f46d663de9208271adeaa429
Niall Arthurs <niall@r1v3r.co.uk>
2016-10-31 16:46:14 +0000

    Simplified website templating process.
    Started to order asset folder.

--- browser.js
+++ browser.js
@@ -10,7 +10,7 @@ var browser = {
       pageContainer.innerHTML = this.websites[url].content;
     } else {
       pageTitle.innerHTML = this.websites["404"].title;
-      websiteSytle.href = this.websites["404"].stylesheet;
+      websiteStyle.href = this.websites["404"].stylesheet;
       pageContainer.innerHTML = this.websites["404"].content;
     }
   },
@@ -38,6 +38,18 @@ var browser = {
     this.websites = websiteDictionary;
     this.homeButton();
   },
+  templateChangePage : function(url) {
+    addressBar.value = url;
+    if (this.websites[url] != undefined) {
+      pageTitle.innerHTML = this.websites[url].title;
+      websiteStyle.href = "../" + this.websites[url].stylesheet;
+      pageContainer.innerHTML = this.websites[url].content;
+    } else {
+      pageTitle.innerHTML = this.websites["404"].title;
+      websiteStyle.href = "../" + this.websites["404"].stylesheet;
+      pageContainer.innerHTML = this.websites["404"].content;
+    }
+  },
   toggleWindowMode : function() {
     if (this.isFullscreen) {
       this.isFullscreen = false;
@@ -60,25 +72,66 @@ var websiteDictionary = {
     title : "Google.com",
     stylesheet : " ",
     content :
-      "<section style='text-align:center;font-size:18pt;margin-top:30px;'>\
-      It's spelt googol!<br>1000000000000000000000000000000000000000000000\
-      0000000000000000000000000000000000000000000000000000000<\section>"
+      "\
+      <section style='text-align:center;font-size:18pt;margin-top:30px;'>\
+        It's spelt googol!\
+        <br>\
+        10000000000000000000000000000000000000000000000000000000000000000000000\
+        000000000000000000000000000000\
+      </section>\
+      "
   },
   "arpa.towerblock.ac/staffportal" : {
     title : "Towerblock Polytechnic Staff Portal",
     stylesheet : "websites/university.css",
     content :
-      "Test"
+      "\
+      <header id = 'universityHeader'>\
+        <section class = 'headerPadding'>\
+          Towerblock\
+          <br>\
+          Polytechnic\
+        </section>\
+        <section id = 'headingContainer'>\
+          <h1 class = 'pageHeading'>\
+            Staff Portal\
+          </h1>\
+          <h2 class = 'tagline'>\
+            Staff Resource Pages\
+          </h2>\
+        </section>\
+        <section class = 'headerPadding'>\
+        </section>\
+      </header>\
+      <nav>\
+      </nav>\
+      "
   },
   "404" : {
     title : "404 Not Found",
     stylesheet : " ",
     content :
-      "<h1 style='font-size: 30pt; margin-top: 5px;'>Not Found</h1>\
-      The resource you are looking for might have been removed, had its name \
-      changed, or is temporarily unavailable.<hr>Please try the following:\
-      <ul><li>If you manually entered the address, make sure that it is \
-      spelled correctly.</li><li>Click the back button to try another link.\
-      </li></ul>"
+      "\
+      <h1 style='font-size: 30pt; margin-top: 5px;'>\
+        Not Found\
+      </h1>\
+      <p>\
+        The resource you are looking for might have been removed, had its name \
+        changed, or is temporarily unavailable.\
+      </p>\
+      <hr>\
+      <p>\
+        Please try the following:\
+      </p>\
+      <ul>\
+        <li>\
+          If you manually entered the address, make sure that it is spelled cor\
+          rectly.\
+        </li>\
+        <li>\
+          Click the back button to try another link.\
+        </li>\
+      </ul>\
+      "
   }
 }
